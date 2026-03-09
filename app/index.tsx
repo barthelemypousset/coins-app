b556b6ab37252a146abca5e660bb0a0dfeee8943
barthelemy pousset <barthelemypousset@gmail.com>
2026-03-09 22:34:54 +0100

    setup expo-router stack nav

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,12 @@
+import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Welcome to coin app !</Text>
+      <Link href={"/coin-detail"}>coin detail</Link>
     </View>
   );
 }
